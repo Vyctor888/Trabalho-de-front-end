e4c7df7ed28a657d00db2e0413f2b3ab25e113ad
Vyctor888 <vkvyctor180@gmail.com>
2024-09-22 17:12:40 -0300

    Initial commit

---
+++ src/Componentes/Imagens.jsx
@@ -0,0 +1,7 @@
+function Imagem () {
+  const img1 = "https://th.bing.com/th/id/OIP.x67YuNtPJBGtsgRvrYgMAAHaEK?rs=1&pid=ImgDetMain"
+  return(
+    <img src={img1} alt='Imgem de The Last of Us'/>
+  )
+}
+export default Imagem
