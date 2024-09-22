35206a2f6250b486ad0985abaad8fa81bf7e4b65
Vyctor888 <vkvyctor180@gmail.com>
2024-09-22 20:25:40 +0000

    add

--- src/Componentes/Imagens.jsx
+++ src/Componentes/Imagens.jsx
@@ -1,7 +1,6 @@
 function Imagem () {
-  const img1 = "https://th.bing.com/th/id/OIP.x67YuNtPJBGtsgRvrYgMAAHaEK?rs=1&pid=ImgDetMain"
   return(
-    <img src={img1} alt='Imgem de The Last of Us'/>
+    <img src="./src/assets/The last uf us imagem"  alt='Imgem de The Last of Us'/>
   )
 }
 export default Imagem
